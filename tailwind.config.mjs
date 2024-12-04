e25b87ff7c8b46aa7a71839acc6be857db77880e
Lostjerome <naronkrach@gmail.com>
2024-12-04 13:05:19 +0700

    chore: Update tailwind.config.mjs to extend colors with primary shades

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    500: "#EB0028",
+                    600: "#C8001D",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
