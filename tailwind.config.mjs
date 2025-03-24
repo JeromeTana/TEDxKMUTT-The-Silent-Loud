7acfa01915ea3a99e74e78f2b3997c104ca0917e
Lostjerome <naronkrach@gmail.com>
2025-03-24 17:43:07 +0700

    refactor: Enhance layout and styling across components, add new SVG icons, and update TypeScript configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        extend: {
-            colors: {
-                primary: {
-                    500: "#EB0028",
-                    600: "#C8001D",
-                },
-            },
-            keyframes: {
-                marquee: {
-                    "0%": { transform: "translateX(0)" },
-                    "100%": { transform: "translateX(-50%)" },
-                },
-            },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: "#EB0028",
+          600: "#C8001D",
         },
+        foreground: "#F7F7F7",
+        darkGray: "#666666",
+        lightGray: "#A4A4A4",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
