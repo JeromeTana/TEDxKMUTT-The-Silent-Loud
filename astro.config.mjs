d8aede0ce70f7b1dc55d31138a764796010681c0
Lostjerome <naronkrach@gmail.com>
2025-03-27 20:04:45 +0700

    feat: Add redirects for language-specific routing and standardize import quotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import sanity from '@sanity/astro';
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/": "/en",
+  },
   integrations: [
     tailwind(),
     icon(),
